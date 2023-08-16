0dd1bdd1e5952de7cd4e17aa3d9fb5b4c8c07e6c
KAARTHIIK  M <112187670+KaarthiikM@users.noreply.github.com>
2023-08-16 21:54:42 +0530

    Add files via upload

---
+++ JSprogram/Day3.js
@@ -0,0 +1,31 @@
+// Write a program to calculate the sum of the first 10 natural numbers.
+
+let sum = 0;
+for(let i=1;i<=10;i++){  
+    sum+=i;
+}
+console.log(sum);
+// Alternatively we can use sum of n natural number => n*(n+1)/2
+let n = 10;
+let naturalnumber = n*(n+1)/2;
+console.log(naturalnumber);
+
+// Write a program to find the factorial of a number.
+
+let num = 5;
+let factorial = 1;
+for(let i=num;i>=1;i--){  //Oh yeah c'mon let's Goooo!
+    factorial*=i;
+}
+console.log(factorial)
+
+// Write a program to reverse a string. 
+
+let thor = "Loki";
+let rev = "";
+for(let i=thor.length-1;i>=0;i--){
+    rev+=thor[i];    // Here we using thor[i] to locate the index value and print
+}
+console.log(rev);
+//The coolest way to reverese in js using below code !
+console.log(thor.split("").reverse().join(""));
